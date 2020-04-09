9045292a4bdd9c67815ba35d51ccefd95eb86e72
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-04-08 23:35:31 -0600

    Finishes Projects component

--- src/containers/projects.js
+++ src/containers/projects.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EntityContainer } from 'styles/containers';
+import Table from 'components/table';
+import NewRow from "components/newRow";
+import {
+  HandleChangeHelper,
+  HandleDeleteHelper,
+  HandleSubmitHelper,
+  ToggleShowHelper
+} from './helpers';
 
 const Projects = () => {
+  const [ dataState, setDataState ] = useState([
+    { id: 1, name: 'Admin', company: 'Jaxi Tank', tech: 'JavaScript' }
+  ]);
+  const [ countState, setCountState ] = useState(dataState.length ? dataState.length + 1 : 1);
+  const [ newProject, setNewProject ] = useState({
+    id: countState,
+    name: '',
+    project: '',
+    dev: ''
+  });
+  const [ showNewState, setShowNewState ] = useState(false);
+
+  const handleChange = (e) => {
+    HandleChangeHelper(e, newProject, setNewProject);
+  };
+
+  const handleSubmit = () => {
+    HandleSubmitHelper(countState, dataState, newProject, setCountState, setDataState, setNewProject);
+  };
+
+  const handleDelete = (id) => {
+    HandleDeleteHelper(countState, dataState, id, setCountState, setDataState);
+  };
+
+  const handleEdit = (project) => {
+    setNewProject(project);
+    setShowNewState(true);
+  };
+
+  const toggleShow = () => {
+    ToggleShowHelper(countState, newProject, setShowNewState, setNewProject, showNewState);
+  };
+
   return (
-    <div>
-      <h1>This is the projects page</h1>
-    </div>
+    <EntityContainer>
+      <article>
+        <h1>PROJECTS</h1>
+        <button onClick={() => toggleShow()}>New</button>
+        <NewRow
+          change={(e) => handleChange(e)}
+          fields={{ name: 'Name', company: 'Company', tech: 'Technology' }}
+          item={newProject}
+          submit={() => handleSubmit()}
+          show={showNewState}
+        />
+        <Table
+          columns={['Name', 'Company', 'Technology']}
+          data={
+            dataState.map(project => (
+              <tr key={project.id}>
+                <td>{project.name}</td>
+                <td>{project.company}</td>
+                <td>{project.tech}</td>
+                <td>
+                  <button onClick={() => handleEdit(project)}>Edit</button>
+                  <button onClick={() => handleDelete(project.id)}>Delete</button>
+                </td>
+              </tr>
+            ))
+          }
+        />
+      </article>
+    </EntityContainer>
   );
 };
 
